74d7c70cd7a4fa931c2b59b99e2b7232e0784594
Andrew Morrison <morrison.andrew422@gmail.com>
2023-06-07 23:25:29 -0400

    setup dynamic list

--- src/pages/MealCardList/MealCardList.tsx
+++ src/pages/MealCardList/MealCardList.tsx
@@ -3,12 +3,13 @@ import { useState, useEffect } from 'react'
 
 // services
 import * as mealCardService from '../../services/mealCardService'
+import * as profileService from '../../services/profileService'
 
 // css
 import styles from './MealCardList.module.css'
 
 // types
-import { MealCard, User } from '../../types/models'
+import { MealCard, User, LikedMeal, PassedMeal } from '../../types/models'
 
 // components
 import MealCardComp from '../../components/MealCardComp/MealCardComp'
@@ -20,12 +21,38 @@ interface MealCardListProps {
 
 const MealCardList = (props: MealCardListProps): JSX.Element => {
   const [mealCards, setMealCards] = useState<MealCard[]>([])
+  const [displayedMealCards, setDisplayedMealCards] = useState<MealCard[]>([])
+  const [likedMeals, setLikedMeals] = useState<LikedMeal[]>([])
+  const [passedMeals, setPassedMeals] = useState<PassedMeal[]>([])
 
-  useEffect((): void => {
+  const filterMealCards = () => {
+    if (likedMeals.length === 0 && passedMeals.length === 0) {
+      return mealCards
+    }
+  
+    const filteredMealCards = mealCards.filter((mealCard: MealCard) => {
+      const likedMealIds = likedMeals.map((likedMeal: LikedMeal) => likedMeal.mealCardId)
+      const passedMealIds = passedMeals.map((passedMeal: PassedMeal) => passedMeal.mealCardId)
+    
+      const isLiked = likedMealIds.includes(mealCard.id)
+      const isPassed = passedMealIds.includes(mealCard.id)
+    
+      return !isLiked && !isPassed
+    })
+    return filteredMealCards
+  }
+
+  const updateMealCards = () => {
+    const filteredMealCards = filterMealCards()
+    setDisplayedMealCards(filteredMealCards)
+  }
+  
+  useEffect(() => {
     const fetchMealCards = async (): Promise<void> => {
       try {
-        const mealCardData: MealCard[] = await mealCardService.getAllMealCards()
-        setMealCards(mealCardData)
+        const likedMeals: LikedMeal[] = await profileService.getLikedMeals(props.user.id)
+        setLikedMeals(likedMeals)
+        updateMealCards()
       } catch (error) {
         console.log(error)
       }
@@ -33,25 +60,55 @@ const MealCardList = (props: MealCardListProps): JSX.Element => {
     fetchMealCards()
   }, [])
 
-  if (!mealCards.length) return (
-  <main className={styles.MealCardList}>
-    <h1>nothing to swipe on</h1>
-  </main>
-  )
-  
+  useEffect(() => {
+    const fetchMealCards = async (): Promise<void> => {
+      try {
+        const passedMeals: PassedMeal[] = await profileService.getPassedMeals(props.user.id)
+        setPassedMeals(passedMeals)
+        updateMealCards()
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchMealCards()
+  }, [])
+
+  useEffect(() => {
+    const fetchMealCards = async (): Promise<void> => {
+      try {
+        const mealCardData: MealCard[] = await mealCardService.getAllMealCards()
+        setMealCards(mealCardData)
+        setDisplayedMealCards(filterMealCards())
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchMealCards()
+  }, [])
+
+
   return (
     <main className={styles.MealCardList}>
-      <h1>Hello. This is a list of all the mealCards.</h1>
-      <NewMealCard mealCards={mealCards} setMealCards={setMealCards}/>
-      {mealCards.map((mealCard: MealCard) => (
-        <MealCardComp 
-        key={mealCard.id} 
-        mealCard={mealCard} 
-        user={props.user} 
-        mealCards={mealCards}
-        setMealCards={setMealCards}
-        />
-        ))}
+      { !mealCards.length
+        ?
+        <h1>nothing to swipe on</h1>
+        :
+        <>
+          <h1>Hello. This is a list of all the mealCards.</h1>
+          <NewMealCard mealCards={mealCards} setMealCards={setMealCards}/>
+          {filterMealCards().map((mealCard: MealCard) => (
+            <MealCardComp 
+              key={mealCard.id} 
+              mealCard={mealCard} 
+              user={props.user} 
+              mealCards={mealCards}
+              setMealCards={setMealCards}
+              updateMealCards={updateMealCards}
+              filterMealCards={filterMealCards}
+            />
+          ))}
+        </>
+      }
     </main>
   )
 }
